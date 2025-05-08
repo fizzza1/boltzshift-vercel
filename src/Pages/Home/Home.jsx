0c38e0f8d738b2cdc23dc8c4bc911018f5113436
akisial <contact.akifsial@gmail.com>
2025-05-08 12:39:09 +0600

    Updated import paths for components

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,16 +1,15 @@
-import React from "react";
-import Header from "../../Components/Header";
-import HeroSection from "../../pages/Components/HeroSection";
-import ServicesSection from "../../pages/Components/ServicesSection";
-import PlansSection from "../../pages/Components/PlansSection";
-import Footer from "../../Components/Footer";
-import TestimonialPage from "../../pages/Components/TestimonialPage";
-import BookingPage from "../../pages/Components/BookingPage";
+import React, { useRef } from "react";
+import Header from "../../Components/Header"; // Header component
+import HeroSection from "../../Components/HeroSection"; // HeroSection from Components folder
+import ServicesSection from "../../Components/ServicesSection"; // ServicesSection from Components folder
+import PlansSection from "../../Components/PlansSection"; // PlansSection from Components folder
+import Footer from "../../Components/Footer"; // Footer component
+import TestimonialPage from "../../Components/TestimonialPage"; // TestimonialPage from Components folder
+import BookingPage from "../../Components/BookingPage"; // BookingPage from Components folder
 
 function Home() {
   // Create a ref for the PlansSection
   const plansRef = useRef(null);
-
 
   const scrollToSection = (sectionId) => {
     const section = document.getElementById(sectionId);
@@ -21,12 +20,11 @@ function Home() {
       });
     }
   };
-  
 
   return (
-    <div className="px-6 ">
+    <div className="px-6">
       <div>
-      <Header scrollToSection={scrollToSection} />
+        <Header scrollToSection={scrollToSection} />
       </div>
       <div id="hero-section">
         <HeroSection />
@@ -44,7 +42,7 @@ function Home() {
         <BookingPage />
       </div>
 
-      <div id="plans-section">
+      <div id="plans-section" ref={plansRef}>
         <PlansSection />
       </div>
 
